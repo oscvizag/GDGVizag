3998235f226ca8c193c3ea789f36042f507f5b07
Samar Khan <36196934+samarkhanp@users.noreply.github.com>
2019-09-22 12:59:28 +0530

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -3,6 +3,20 @@
  	easing: 'slide'
  });
 
+ $(function() {
+	var selectedClass = "";
+	$(".filter").click(function(){
+	selectedClass = $(this).attr("data-rel");
+	$("#gallery").fadeTo(100, 0.1);
+	$("#gallery div").not("."+selectedClass).fadeOut().removeClass('animation');
+	setTimeout(function() {
+	$("."+selectedClass).fadeIn().addClass('animation');
+	$("#gallery").fadeTo(300, 1);
+	}, 300);
+	});
+	});
+	
+
 (function($) {
 
 	"use strict";
@@ -268,6 +282,9 @@
 
     fixedContentPos: false
   });
+
+  
+   
 
 
 
